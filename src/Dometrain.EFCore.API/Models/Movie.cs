67fb05263010cfdefee2cb335d3e4e1aa6206f72
Hannes Lowette <lowette.hannes@gmail.com>
2023-08-11 16:37:23 +0200

    Post - 4.2

--- src/Dometrain.EFCore.API/Models/Movie.cs
+++ src/Dometrain.EFCore.API/Models/Movie.cs
@@ -1,10 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Dometrain.EFCore.API.Models;
 
+[Table("Pictures")]
 public class Movie
 {
+    [Key]
     public int Identifier { get; set; }
-    public string? Title { get; set; }    
+    
+    [MaxLength(128)]
+    [Column(TypeName = "varchar")]
+    [Required]
+    public string? Title { get; set; }  
+    
+    [Column(TypeName = "date")]
     public DateTime ReleaseDate { get; set; }
+    
+    [Column("Plot", TypeName = "varchar(max)")]
     public string? Synopsis { get; set; }
 }
 
