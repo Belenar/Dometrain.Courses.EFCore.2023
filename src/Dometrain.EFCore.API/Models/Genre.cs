738a25ce1858fb3a4432131c9f9ff8917830fe93
Hannes Lowette <lowette.hannes@gmail.com>
2023-08-12 22:21:00 +0200

    Post - 4.7

--- src/Dometrain.EFCore.API/Models/Genre.cs
+++ src/Dometrain.EFCore.API/Models/Genre.cs
@@ -8,5 +8,8 @@ public class Genre
     public string Name { get; set; } = string.Empty;
 
     [JsonIgnore]
+    public DateTime CreatedDate { get; set; }
+
+    [JsonIgnore]
     public ICollection<Movie> Movies { get; set; } = new HashSet<Movie>();
 }
