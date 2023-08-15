43905d85637dfc9a3eccaa1567af406a9ebf0b74
Hannes Lowette <lowette.hannes@gmail.com>
2023-08-15 02:11:15 +0200

    Pre - 6.4

---
+++ src/Dometrain.EFCore.Tests/FakeDbSet/FakeDbSetTest.cs
@@ -0,0 +1,40 @@
+using Dometrain.EFCore.SimpleAPI.Controllers;
+using Dometrain.EFCore.SimpleAPI.Data;
+using Dometrain.EFCore.SimpleAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+
+namespace Dometrain.EFCore.Tests.FakeDbSet;
+
+public class FakeDbSetTest
+{
+    [Fact]
+    public async Task IfGenreExists_ReturnsGenre()
+    {
+        // Arrange
+        var context = CreateFakeDbContext();
+        var controller = new GenresController(context);
+        
+        // Act
+        var response = await controller.Get(2);
+        var okResult = response as OkObjectResult;
+        
+        // Assert
+        Assert.NotNull(okResult);
+        Assert.Equal(200, okResult.StatusCode);
+        Assert.Equal("Action", (okResult.Value as Genre)?.Name);
+        await context.DidNotReceive().SaveChangesAsync();
+    }
+
+    private MoviesContext CreateFakeDbContext()
+    {
+        List<Genre> genres = new()
+        {
+            new Genre { Id = 1, Name = "Drama"},
+            new Genre { Id = 2, Name = "Action"},
+            new Genre { Id = 3, Name = "Comedy"}
+        };
+
+        throw new NotImplementedException();
+    }
+}
