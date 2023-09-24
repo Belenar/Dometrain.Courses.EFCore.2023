9ce46ca7232392c1410915cb17101b6882f2c043
Hannes Lowette <lowette.hannes@gmail.com>
2023-09-24 17:00:04 +0200

    Post - 8.5

--- src/Dometrain.EFCore.API/Data/EntityMapping/ActorMapping.cs
+++ src/Dometrain.EFCore.API/Data/EntityMapping/ActorMapping.cs
@@ -8,5 +8,21 @@ public class ActorMapping : IEntityTypeConfiguration<Actor>
 {
     public void Configure(EntityTypeBuilder<Actor> builder)
     {
+        builder.HasMany(actor => actor.Movies)
+            .WithMany(movie => movie.Actors)
+            .UsingEntity("Movie_Actor",
+                left => left.HasOne(typeof(Movie))
+                    .WithMany()
+                    .HasForeignKey("MovieId")
+                    .HasPrincipalKey(nameof(Movie.Identifier))
+                    .HasConstraintName("FK_MovieActor_Movie")
+                    .OnDelete(DeleteBehavior.Cascade),
+                right => right.HasOne(typeof(Actor))
+                    .WithMany()
+                    .HasForeignKey("ActorId")
+                    .HasPrincipalKey(nameof(Actor.Id))
+                    .HasConstraintName("FK_MovieActor_Actor")
+                    .OnDelete(DeleteBehavior.Cascade),
+                linkBuilder => linkBuilder.HasKey("MovieId", "ActorId"));
     }
 }
--- src/Dometrain.EFCore.API/Models/Actor.cs
+++ src/Dometrain.EFCore.API/Models/Actor.cs
@@ -5,4 +5,6 @@ public class Actor
     public int Id { get; set; }
     public required string FirstName { get; set; }
     public required string LastName { get; set; }
+
+    public List<Movie> Movies { get; set; } = new();
 }
