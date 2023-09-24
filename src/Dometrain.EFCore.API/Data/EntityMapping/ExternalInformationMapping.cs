08d9fb03f9376683aa7c482f418cd3c6436a1958
Hannes Lowette <lowette.hannes@gmail.com>
2023-09-24 13:55:52 +0200

    Post - 8.4

--- src/Dometrain.EFCore.API/Data/EntityMapping/ExternalInformationMapping.cs
+++ src/Dometrain.EFCore.API/Data/EntityMapping/ExternalInformationMapping.cs
@@ -8,5 +8,9 @@ public class ExternalInformationMapping : IEntityTypeConfiguration<ExternalInfor
 {
     public void Configure(EntityTypeBuilder<ExternalInformation> builder)
     {
+        builder.HasKey(info => info.MovieId);
+
+        builder.HasOne(info => info.Movie)
+            .WithOne(movie => movie.ExternalInformation);
     }
 }
---
+++ src/Dometrain.EFCore.API/Migrations/20230924113853_AddOneToOne.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Dometrain.EFCore.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddOneToOne : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ExternalInformations",
+                columns: table => new
+                {
+                    MovieId = table.Column<int>(type: "int", nullable: false),
+                    ImdbUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    RottenTomatoesUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TmdbUrl = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ExternalInformations", x => x.MovieId);
+                    table.ForeignKey(
+                        name: "FK_ExternalInformations_Pictures_MovieId",
+                        column: x => x.MovieId,
+                        principalTable: "Pictures",
+                        principalColumn: "Identifier",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ExternalInformations");
+        }
+    }
+}
