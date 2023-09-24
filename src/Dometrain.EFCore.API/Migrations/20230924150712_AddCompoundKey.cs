9127f1cf9f19baf34627ea0435b85266ae44363d
Hannes Lowette <lowette.hannes@gmail.com>
2023-09-24 20:11:53 +0200

    Post - 8.6

---
+++ src/Dometrain.EFCore.API/Migrations/20230924150712_AddCompoundKey.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Dometrain.EFCore.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCompoundKey : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddUniqueConstraint(
+                name: "AK_Pictures_Title_ReleaseDate",
+                table: "Pictures",
+                columns: new[] { "Title", "ReleaseDate" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropUniqueConstraint(
+                name: "AK_Pictures_Title_ReleaseDate",
+                table: "Pictures");
+        }
+    }
+}
