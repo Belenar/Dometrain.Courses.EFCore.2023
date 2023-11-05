cb3a19c4afc5e3858e0f3d763a6909f7c27ae63c
Hannes Lowette <lowette.hannes@gmail.com>
2023-11-05 14:20:08 +0100

    Model type fix

--- src/Dometrain.EFCore.API/Models/Movie.cs
+++ src/Dometrain.EFCore.API/Models/Movie.cs
@@ -4,7 +4,7 @@ public class Movie
 {
     public int Identifier { get; set; }
     public string? Title { get; set; }
-    public DateOnly ReleaseDate { get; set; }
+    public DateTime ReleaseDate { get; set; }
     public string? Synopsis { get; set; }
     public AgeRating AgeRating { get; set; }
 
