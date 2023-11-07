bfbaeec34759c881b0a0c9c03fe34e9252ce4655
Hannes Lowette <lowette.hannes@gmail.com>
2023-11-07 16:50:38 +0100

    Post - 3.2

---
+++ src/Dometrain.EFCore.API/Data/MoviesContext.cs
@@ -0,0 +1,9 @@
+using Dometrain.EFCore.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dometrain.EFCore.API.Data;
+
+public class MoviesContext : DbContext
+{
+    public DbSet<Movie> Movies => Set<Movie>();
+}
