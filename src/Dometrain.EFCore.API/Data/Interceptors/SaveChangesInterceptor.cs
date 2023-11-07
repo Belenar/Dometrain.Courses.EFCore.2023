e3db3de3373ca8b91e45c13b15050eceef346b8d
Hannes Lowette <lowette.hannes@gmail.com>
2023-11-07 22:19:33 +0100

    Post - 8.9

--- src/Dometrain.EFCore.API/Data/Interceptors/SaveChangesInterceptor.cs
+++ src/Dometrain.EFCore.API/Data/Interceptors/SaveChangesInterceptor.cs
@@ -1,15 +1,36 @@
+using Dometrain.EFCore.API.Models;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Diagnostics;
 
 namespace Dometrain.EFCore.API.Data.Interceptors;
 
 public class SaveChangesInterceptor : ISaveChangesInterceptor
 {
-    public InterceptionResult<int> SavingChanges(DbContextEventData eventData, InterceptionResult<int> result)
+    public InterceptionResult<int> SavingChanges(
+        DbContextEventData eventData, 
+        InterceptionResult<int> result)
     {
-        throw new NotImplementedException();
+        var context = eventData.Context as MoviesContext;
+
+        if (context is null) return result;
+
+        var tracker = context.ChangeTracker;
+
+        var deleteEntries = tracker.Entries<Genre>()
+            .Where(entry => entry.State == EntityState.Deleted);
+
+        foreach (var deleteEntry in deleteEntries)
+        {
+            deleteEntry.Property<bool>("Deleted").CurrentValue = true;
+            deleteEntry.State = EntityState.Modified;
+        }
+
+        return result;
     }
 
-    public ValueTask<InterceptionResult<int>> SavingChangesAsync(DbContextEventData eventData, InterceptionResult<int> result,
+    public ValueTask<InterceptionResult<int>> SavingChangesAsync(
+        DbContextEventData eventData, 
+        InterceptionResult<int> result,
         CancellationToken cancellationToken = new CancellationToken())
     {
         return ValueTask.FromResult(SavingChanges(eventData, result));
