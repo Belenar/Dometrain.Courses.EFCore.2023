e139c14205be782a8d1f8014ccf1470d4c9f7c62
Hannes Lowette <lowette.hannes@gmail.com>
2023-11-07 18:09:06 +0100

    Post - 5.3

--- src/Dometrain.EFCore.API/Data/EntityMapping/MovieMapping.cs
+++ src/Dometrain.EFCore.API/Data/EntityMapping/MovieMapping.cs
@@ -2,6 +2,7 @@ using Dometrain.EFCore.API.Data.ValueConverters;
 using Dometrain.EFCore.API.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Dometrain.EFCore.API.Data.EntityMapping;
 
@@ -11,7 +12,7 @@ public class MovieMapping : IEntityTypeConfiguration<Movie>
     {
         builder
             .ToTable("Pictures")
-            .HasQueryFilter(movie => movie.ReleaseDate >= new DateTime(1990,1,1))
+            .HasQueryFilter(movie => movie.ReleaseDate > new DateTime(1990,1,1))
             .HasKey(movie => movie.Identifier);
 
         builder.Property(movie => movie.Title)
@@ -27,40 +28,17 @@ public class MovieMapping : IEntityTypeConfiguration<Movie>
             .HasColumnType("varchar(max)")
             .HasColumnName("Plot");
 
-        builder.Property(movie => movie.AgeRating)
-            .HasColumnType("varchar(32)")
-            .HasConversion<string>();
-
-        builder.OwnsOne(movie => movie.Director)
-            .ToTable("Movie_Directors");
-        
-        builder.OwnsMany(movie => movie.Actors)
-            .ToTable("Movie_Actors");
-
         builder
             .HasOne(movie => movie.Genre)
             .WithMany(genre => genre.Movies)
             .HasPrincipalKey(genre => genre.Id)
             .HasForeignKey(movie => movie.MainGenreId);
+
+        builder
+            .OwnsOne(movie => movie.Director)
+            .ToTable("Pictures_Directors"); 
         
-        // Seed - data that needs to be created always
-        builder.HasData(new Movie
-        {
-            Identifier = 1,
-            Title = "Fight Club",
-            ReleaseDate = new DateTime(1999, 9, 10),
-            Synopsis = "Ed Norton and Brad Pitt have a couple of fist fights with each other.",
-            MainGenreId = 1,
-            AgeRating = AgeRating.Adolescent
-        });
-
-        builder.OwnsOne(movie => movie.Director)
-            .HasData(new { MovieIdentifier = 1, FirstName = "David", LastName = "Fincher" });
-
-        builder.OwnsMany(movie => movie.Actors)
-            .HasData(
-                new { MovieIdentifier = 1, Id = 1, FirstName = "Edward", LastName = "Norton" },
-                new { MovieIdentifier = 1, Id = 2, FirstName = "Brad", LastName = "Pitt" }
-            );
+        builder
+            .OwnsMany(movie => movie.Actors);
     }
 }
---
+++ src/Dometrain.EFCore.API/Migrations/20231107170223_InitialSchema.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Dometrain.EFCore.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialSchema : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Genres",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Genres", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pictures",
+                columns: table => new
+                {
+                    Identifier = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "varchar(128)", maxLength: 128, nullable: false),
+                    ReleaseDate = table.Column<string>(type: "char(8)", nullable: false),
+                    Plot = table.Column<string>(type: "varchar(max)", nullable: true),
+                    AgeRating = table.Column<int>(type: "int", nullable: false),
+                    MainGenreId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pictures", x => x.Identifier);
+                    table.ForeignKey(
+                        name: "FK_Pictures_Genres_MainGenreId",
+                        column: x => x.MainGenreId,
+                        principalTable: "Genres",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pictures_Actors",
+                columns: table => new
+                {
+                    MovieIdentifier = table.Column<int>(type: "int", nullable: false),
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pictures_Actors", x => new { x.MovieIdentifier, x.Id });
+                    table.ForeignKey(
+                        name: "FK_Pictures_Actors_Pictures_MovieIdentifier",
+                        column: x => x.MovieIdentifier,
+                        principalTable: "Pictures",
+                        principalColumn: "Identifier",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pictures_Directors",
+                columns: table => new
+                {
+                    MovieIdentifier = table.Column<int>(type: "int", nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pictures_Directors", x => x.MovieIdentifier);
+                    table.ForeignKey(
+                        name: "FK_Pictures_Directors_Pictures_MovieIdentifier",
+                        column: x => x.MovieIdentifier,
+                        principalTable: "Pictures",
+                        principalColumn: "Identifier",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pictures_MainGenreId",
+                table: "Pictures",
+                column: "MainGenreId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Pictures_Actors");
+
+            migrationBuilder.DropTable(
+                name: "Pictures_Directors");
+
+            migrationBuilder.DropTable(
+                name: "Pictures");
+
+            migrationBuilder.DropTable(
+                name: "Genres");
+        }
+    }
+}
